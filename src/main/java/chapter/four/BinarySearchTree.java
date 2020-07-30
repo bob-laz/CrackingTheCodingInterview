37fd2aa3a504d542dc115c92b1d95cd44612e7c1
blaskowski94 <blaskowski94@gmail.com>
2020-07-30 07:48:03 -0500

    improve impl of BST, add contains method

--- src/main/java/chapter/four/BinarySearchTree.java
+++ src/main/java/chapter/four/BinarySearchTree.java
@@ -1,48 +1,73 @@
 package chapter.four;
 
+/**
+ * This binary search tree implementation allows for duplicate values and inserts them to the left.
+ */
 public class BinarySearchTree {
 
     Node root;
 
+    // worst case time complexity O(n) where tree is completely unbalanced (all nodes on one side of root)
+    // best case for balanced tree O(log(n)) where log(n) = height of tree
     public void insert(int data) {
-        if (root == null) root = new Node(data);
-        else searchAndInsert(data, root);
+        root = recursiveInsert(data, root);
     }
 
-    private void searchAndInsert(int data, Node n) {
-        if (data <= n.data) {
-            if (n.left == null) {
-                n.left = new Node(data);
-            } else {
-                searchAndInsert(data, n.left);
-            }
+    private Node recursiveInsert(int data, Node n) {
+        if (n == null) {
+            // found leaf, insert
+            n = new Node(data);
+        } else if (data <= n.data) {
+            // recurse left
+            n.left = recursiveInsert(data, n.left);
         } else {
-            if (n.right == null) {
-                n.right = new Node(data);
-            } else {
-                searchAndInsert(data, n.right);
-            }
+            // recurse right
+            n.right = recursiveInsert(data, n.right);
         }
+        return n;
     }
 
-    private String traversePreOrder(Node root) {
+    // worst case time complexity O(n) where tree is completely unbalanced (all nodes on one side of root) and element not fond
+    // average case for balanced tree O(log(n)) where log(n) = height of tree
+    // best case is O(1) where root is target element
+    public boolean contains(int data) {
+        return recursiveContains(data, root);
+    }
+
+    public boolean recursiveContains(int data, Node n) {
+        if (n == null) {
+            // value not found, false
+            return false;
+        } else if (n.data == data) {
+            // value found, true
+            return true;
+        }
+        // recurse left or right depending on value
+        return data <= n.data ? recursiveContains(data, n.left) : recursiveContains(data, n.right);
+    }
+
+    /**
+     * Note that on a windows PC I had to run my program with the -Dfile.encoding=UTF-8 VM argument to get the box
+     * drawing symbols to display correctly.
+     */
+    public void printTree() {
         if (root == null) {
-            return "";
+            System.out.println("Tree is empty");
+        } else {
+            StringBuilder sb = new StringBuilder();
+            sb.append(root.data);
+
+            String pointerRight = "└──";
+            String pointerLeft = (root.right != null) ? "├──" : "└──";
+
+            recursivePrintHelper(sb, "", pointerLeft, root.left, root.right != null);
+            recursivePrintHelper(sb, "", pointerRight, root.right, false);
+
+            System.out.println(sb.toString());
         }
-
-        StringBuilder sb = new StringBuilder();
-        sb.append(root.data);
-
-        String pointerRight = "└──";
-        String pointerLeft = (root.right != null) ? "├──" : "└──";
-
-        traverseNodes(sb, "", pointerLeft, root.left, root.right != null);
-        traverseNodes(sb, "", pointerRight, root.right, false);
-
-        return sb.toString();
     }
-    private void traverseNodes(StringBuilder sb, String padding, String pointer, Node node,
-                               boolean hasRightSibling) {
+
+    private void recursivePrintHelper(StringBuilder sb, String padding, String pointer, Node node, boolean hasRightSibling) {
         if (node != null) {
             sb.append(System.lineSeparator());
             sb.append(padding);
@@ -60,14 +85,9 @@ public class BinarySearchTree {
             String pointerRight = "└──";
             String pointerLeft = (node.right != null) ? "├──" : "└──";
 
-            traverseNodes(sb, paddingForBoth, pointerLeft, node.left, node.right != null);
-            traverseNodes(sb, paddingForBoth, pointerRight, node.right, false);
+            recursivePrintHelper(sb, paddingForBoth, pointerLeft, node.left, node.right != null);
+            recursivePrintHelper(sb, paddingForBoth, pointerRight, node.right, false);
         }
-
-    }
-
-    public void print() {
-        System.out.println(traversePreOrder(root));
     }
 
     public static class Node {
--- src/test/java/chapter/four/BinarySearchTreeTest.java
+++ src/test/java/chapter/four/BinarySearchTreeTest.java
@@ -1,25 +1,66 @@
 package chapter.four;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class BinarySearchTreeTest {
 
-    @BeforeAll
-    public static void setup() {
-        System.setProperty("file.encoding", "UTF-8");
+    @Test
+    public void binarySearchTreeInsertTest() {
+        BinarySearchTree bst = new BinarySearchTree();
+        bst.insert(8);
+        bst.insert(3);
+        bst.insert(10);
+        bst.insert(1);
+        bst.insert(6);
+        bst.insert(14);
+        bst.insert(4);
+        bst.insert(7);
+        bst.insert(13);
+
+        bst.printTree();
+
+        BinarySearchTree bst2 = new BinarySearchTree();
+        bst2.insert(8);
+        bst2.insert(14);
+        bst2.insert(10);
+        bst2.insert(1);
+        bst2.insert(6);
+        bst2.insert(13);
+        bst2.insert(3);
+        bst2.insert(4);
+        bst2.insert(7);
+
+        bst2.printTree();
     }
 
     @Test
-    public void binaryTreeTest() {
+    public void binarySearchTreeContainsTest() {
         BinarySearchTree bst = new BinarySearchTree();
-        bst.insert(5);
-        bst.insert(6);
-        bst.insert(4);
+        bst.insert(8);
+        bst.insert(3);
         bst.insert(10);
         bst.insert(1);
+        bst.insert(6);
+        bst.insert(14);
+        bst.insert(4);
+        bst.insert(7);
+        bst.insert(13);
 
-        bst.print();
+        assertTrue(bst.contains(8));
+        assertTrue(bst.contains(3));
+        assertTrue(bst.contains(10));
+        assertTrue(bst.contains(1));
+        assertTrue(bst.contains(6));
+        assertTrue(bst.contains(14));
+        assertTrue(bst.contains(4));
+        assertTrue(bst.contains(7));
+        assertTrue(bst.contains(13));
+        assertFalse(bst.contains(146));
+        assertFalse(bst.contains(152));
+        assertFalse(bst.contains(999));
     }
 
 }
